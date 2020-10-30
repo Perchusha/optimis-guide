6907dbb1434aad513a725930ad5ae4434c7301da
Perchusha <perch.anton@gmail.com>
2020-10-30 15:17:41 +0300

    [pretty] Change Suite CDN directive

--- src/views/TopLayout.js
+++ src/views/TopLayout.js
@@ -2,6 +2,6 @@ import { View } from "dhx-optimus";
 
 export class TopLayout extends View {
 	init() {
-		return `<h3 style="padding-left: 20px;">Hello ${this.app.state.name}!</h3>`;
+		return `<h3 style="padding: 20px; margin: 0;">Hello ${this.app.state.name}!</h3>`;
 	}
 }
