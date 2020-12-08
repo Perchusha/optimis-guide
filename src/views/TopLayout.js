616938bbf619f1e99b0a38308eff17d4a5d4bb0f
Perchusha <perch.anton@gmail.com>
2020-12-08 18:39:10 +0300

    Update styles

--- src/views/TopLayout.js
+++ src/views/TopLayout.js
@@ -2,6 +2,6 @@ import { View } from "dhx-optimus";
 
 export class TopLayout extends View {
 	init() {
-		return `<h1 style="margin: 100px; text-align: center;">Hello ${this.app.state.name}!</h1>`;
+		return `<h1>Hello ${this.app.state.name}!</h1>`;
 	}
 }
