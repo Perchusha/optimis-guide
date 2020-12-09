ac0cbd48c3daf0c733006dc66fa92d91a330a974
Sorokin-Oleg <sorokin.world@gmail.com>
2020-12-09 16:03:32 +0300

    [dev] Fixed some problems

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import "./assets/css/index.css";
 
 import { App } from "dhx-optimus";
-import Store from "dhx-optimus-store";
 
 import { TopLayout } from "./views/TopLayout";
 
-const initialState = {
-	name: "Optimus",
-};
-
 export class MyApp extends App {
 	init() {
-		this.store = new Store(initialState);
-		this.params.store = this.store;
-		this.state = this.store.getState();
 		this.show(null, TopLayout);
 	}
 }
