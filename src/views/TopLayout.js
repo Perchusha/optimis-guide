fc10cb2ed55bc975bccdb163ef16061969a95ec1
Sorokin-Oleg <sorokin.world@gmail.com>
2020-12-10 16:10:44 +0300

    [dev] Update initial step

--- src/views/TopLayout.js
+++ src/views/TopLayout.js
@@ -2,6 +2,6 @@ import { View } from "dhx-optimus";
 
 export class TopLayout extends View {
 	init() {
-		return `<h1>Hello ${this.app.state.name}!</h1>`;
+		return `<h1>Hello Optimus!</h1>`;
 	}
 }
